b0be6afe2d7de540b7a7cb106062b8beea9730c5
avejohnsen <ajohnsen@meca.edu>
2024-12-06 15:32:50 -0500

    artist_update

--- home.js
+++ home.js
@@ -1,5 +1,3 @@
 window.onload = function() {
-    var element = document.getElementById('title_img');
-    var position = element.offsetTop - 100; // Adjust this value as needed
-    window.scrollTo(0, position);
-};
+    window.scrollTo()
+}
